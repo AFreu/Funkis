218da4baceedd4e7c1fc6f95a99d4558b307e2d5
Henning Freudenthaler <Libra@dhcp-228-086.nomad.chalmers.se>
2015-11-09 11:27:03 +0100

    Lab2A done

--- BlackJack.hs
+++ BlackJack.hs
@@ -1,6 +1,9 @@
 module BlackJack where
 import Cards
 import Wrapper
+import Test.QuickCheck
+--------------- Lab 2A ----------------------------------
+-- Group 10 
 
 {- size hand2 = size (Add (Card(Numeric 2) Hearts)
                     (Add (Card Jack Spades) Empty)
@@ -10,42 +13,78 @@ import Wrapper
         2
 -}
 
+-- Returns the empty hand.
 empty :: Hand
 empty = Empty 
 
-hand1 = (Add(Card(Numeric 2) Hearts)(Add (Card Jack Spades) Empty))
-hand2 = (Add(Card(Numeric 3) Hearts)(Add (Card Jack Hearts) Empty))
-acehand = (Add(Card Ace Hearts)(Add(Card Ace Clubs)(Add(Card Ace Diamonds)(Add(Card Ace Spades) Empty))))
-
--- case number of ace
+-- Gives the total value of a Hand of Cards,
+-- taking aces into account.
 value :: Hand -> Integer
-value h | valueHand h > 21  = valueHand h -10*numberOfAces h
-        | otherwise = valueHand h
-                
+value h | tot > 21  = tot - (10 * numberOfAces h)
+        | otherwise = tot
+        where tot = valueHand h
+ 
+-- Gives the value of a Hand where Aces use default value 11. 
 valueHand :: Hand -> Integer
 valueHand Empty = 0
 valueHand (Add c h) = (valueCard c) + (valueHand h) 
-                
-valueRank :: Rank -> Integer
-valueRank King = 10
-valueRank Queen = 10
-valueRank Jack = 10
-valueRank (Numeric n) = n 
-valueRank _ = 11
 
--- valueCard acquires the value based on the card rank
--- and Aces will either return one or 11.
+-- Gives a value for the input Rank
+valueRank :: Rank -> Integer
+valueRank Ace = 11
+valueRank (Numeric n) = n 
+valueRank _ = 10
+
+-- Acquires the value from a given card.
 valueCard :: Card -> Integer
 valueCard (Card r _) = valueRank r 
 
+-- Returns the amount of aces in the given hand
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add ( Card Ace _ ) h) = 1 + numberOfAces h 
 numberOfAces (Add ( Card _ _ ) h)   = numberOfAces h
 
+-- Returns whether the player is bust'd
 gameOver :: Hand -> Bool
 gameOver h = value h > 21
 
+-- Compares the player hand to the bank to distinguish the winner.
 winner :: Hand -> Hand -> Player
 winner p b  | value p > value b = Guest
             | otherwise         = Bank
+  
+    
+----------------Lab 2B----------------------------------------
+  
+
+(<+) :: Hand -> Hand -> Hand
+h1          <+ Empty        = h1
+Empty       <+ h2           = h2
+Add c1 h1   <+ h2           = Add c1 (h1 <+ h2)
+
+
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 = p1 <+ (p2 <+ p3) == (p1 <+ p2) <+ p3
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 = size (p1 <+ p2) == size p1 + size p2
+
+fullDeck :: Hand 
+fullDeck = suitCards Hearts <+ suitCards Clubs
+             <+ suitCards Spades <+ suitCards Diamonds
+
+suitCards :: Suit -> Hand
+suitCards s1 =  suitNumeric s1 <+ (Add(Card Jack s1)
+                                  (Add(Card Queen s1)
+                                  (Add(Card King s1) 
+                                        Empty)))
+
+suitNumeric :: Suit -> Hand
+suitNumeric s1 = suitNumeric1 s1 2
+
+suitNumeric1 :: Suit -> Integer -> Hand 
+suitNumeric1 s1 11 = Empty
+suitNumeric1 s1 n = Add (Card (Numeric n) s1) Empty 
+                    <+ suitNumeric1 s1 (n+1)
+
